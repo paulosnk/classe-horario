0a458d75a55e806e30d60e612c826b9334bfd475
Paulo <pauloutira@hotmail.com>
2021-06-13 09:17:19 -0300

    Classe-horario git

---
+++ Classe Horario/src/Horario.java
@@ -0,0 +1,12 @@
+public class Horario {
+				private Byte hour;
+			private Byte minute;
+		private Byte second;
+	
+		public Horario() {}
+	public Horario(Byte hour, Byte minute, Byte second) {
+		setHour(hour);
+		setMinute(minute);
+		setSecond(second);
+	}
+}
